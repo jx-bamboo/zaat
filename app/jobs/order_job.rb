bfa682f30e5911fcba64357520ebbf93b9f6bb76
jx-bamboo <jx-bamboo@outlook.com>
2024-07-05 23:09:06 +0800

    update order job bsckey

--- app/jobs/order_job.rb
+++ app/jobs/order_job.rb
@@ -22,7 +22,8 @@ class OrderJob < ApplicationJob
   private
 
   def call_bsc_api(txhash)
-    bsc_key = "1M5JQRT1W4B4DYBBKTPYD1WMMHGIU9T8G9"
+    # bsc_key = "1M5JQRT1W4B4DYBBKTPYD1WMMHGIU9T8G9"
+    bsc_key = "YourApiKeyToken"
     uri = "https://api-testnet.bscscan.com/api?module=account&action=txlistinternal&txhash=#{txhash}&apikey=#{bsc_key}"
     response = Faraday.get(uri)
     
