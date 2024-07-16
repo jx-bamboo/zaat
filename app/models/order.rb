e241e5689129d53b7e444ae97f767d398916b1bd
jx-bamboo <jx-bamboo@outlook.com>
2024-07-16 11:06:15 +0800

    update order new controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,10 +6,6 @@ class Order < ApplicationRecord
 
   enum status: [:pending, :creating, :completed]
 
-  scope :my_order_pendding, -> { where(status: 0).order(created_at: :desc).limit(3)}
   scope :not_success, -> { where.not(status: 2).order(created_at: :desc)}
 
-  # def pending?
-  #   status == 0
-  # end
 end
